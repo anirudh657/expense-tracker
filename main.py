d6270b2fe2251a87eec94462e12fc40f996170a3
Anirudh <anirudharoraarora2@gmail.com>
2026-07-23 22:11:05 +0530

    Added expense tracker add expense feature

---
+++ main.py
@@ -0,0 +1,60 @@
+expenses = []
+
+
+def add_expense():
+    print("\n----- Add Expense -----")
+
+    category = input("Enter Category: ")
+    amount = float(input("Enter Amount: "))
+    note = input("Enter Note: ")
+
+    expense = {
+        "category": category,
+        "amount": amount,
+        "note": note
+    }
+
+    expenses.append(expense)
+
+    print("\n✅ Expense Added Successfully!")
+
+
+def menu():
+    print("\n" + "=" * 40)
+    print("      EXPENSE TRACKER")
+    print("=" * 40)
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. Search Expense")
+    print("4. Delete Expense")
+    print("5. Total Expense")
+    print("6. Exit")
+
+
+while True:
+    menu()
+
+    choice = input("Enter your choice: ").strip()
+
+    if choice == "1":
+        add_expense()
+
+    elif choice == "2":
+        print("✅ View Expense Selected")
+
+    elif choice == "3":
+        print("✅ Search Expense Selected")
+
+    elif choice == "4":
+        print("✅ Delete Expense Selected")
+
+    elif choice == "5":
+        print("✅ Total Expense Selected")
+
+    elif choice == "6":
+        print("👋 Thank You!")
+        break
+
+    else:
+     print("Invalid Choice")
+        
