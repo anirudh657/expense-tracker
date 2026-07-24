d6794f60c12e390a9c3f6ed01dbc4e7cb12ea975
Anirudh <anirudharoraarora2@gmail.com>
2026-07-24 22:18:16 +0530

    adding delete feature

--- main.py
+++ main.py
@@ -6,23 +6,67 @@ expenses = []
 def add_expense():
     print("\n----- Add Expense -----")
 
+    date = input("Enter Date (DD-MM-YYYY): ")
     category = input("Enter Category: ")
     amount = float(input("Enter Amount: "))
     note = input("Enter Note: ")
 
-    expense = {}
-
-    expense["category"] = category
-    expense["amount"] = amount
-    expense["note"] = note
+    expense = {
+        "date": date,
+        "category": category,
+        "amount": amount,
+        "note": note
+    }
 
     expenses.append(expense)
 
-    print("Expense Added Successfully!")
+    print("\nExpense Added Successfully!")
 
+  
+
+
+
+   
+
+    
+
+
+
+def delete_expense():
+    print("\n----- Delete Expense -----")
+
+    if len(expenses) == 0:
+        print("No Expenses Available.")
+        return
+
+    print("\nExpense List:")
+
+    number = 1
+
+    for expense in expenses:
+        print(number)
+        print("Date :", expense["date"])
+        print("Category :", expense["category"])
+        print("Amount :", expense["amount"])
+        print("Note :", expense["note"])
+        print("-----------------------")
+
+        number = number + 1
+
+    
+
+    
+
+choice = int(input("Enter Expense Number to Delete: "))
+
+if 1 <= choice <= len(expenses):
+    expenses.pop(choice - 1)
+    print("Expense Deleted Successfully!")
+else:
+    print("Invalid Expense Number!")
 
 def menu():
-    print("\n" + "=" * 40)
+    print("=" * 40)
     print("      EXPENSE TRACKER")
     print("=" * 40)
     print("1. Add Expense")
@@ -36,27 +80,29 @@ def menu():
 while True:
     menu()
 
-    choice = input("Enter your choice: ").strip()
+    choice = input("Enter your choice: ")
+    print("You entered:", choice)
 
     if choice == "1":
         add_expense()
 
     elif choice == "2":
-        print("✅ View Expense Selected")
+        print("View Expense Selected")
 
     elif choice == "3":
-        print("✅ Search Expense Selected")
+        print("Search Expense Selected")
 
     elif choice == "4":
-        print("✅ Delete Expense Selected")
+        print("DEBUG: Inside option 4")
+        delete_expense()
 
     elif choice == "5":
-        print("✅ Total Expense Selected")
+        print("Total Expense Selected")
 
     elif choice == "6":
-        print("👋 Thank You!")
+        print("Thank You!")
         break
 
     else:
-     print("Invalid Choice")
-        
+        print("Invalid Choice")
+
